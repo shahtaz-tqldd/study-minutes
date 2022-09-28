55e77d366668d0852a71504d3173f5b8babf39d6
Shahtaz Rahman <shahtazrahman17@gmail.com>
2022-09-29 03:32:39 +0600

    font-lato, card added and reload in atudy-room

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -2,10 +2,14 @@ import React from 'react'
 import './course.css'
 
 const Course = (props) => {
-    const {courseName} = props.course
+    const {courseName, image, topics, time} = props.course
   return (
-    <div>
-        <h3>{courseName}</h3>
+    <div className='course-card'>
+        <img src={image} alt={courseName} />
+        <h2>{courseName}</h2>
+        <div className='topics'>{topics.map(topic => <span> {topic} </span>)}</div>
+        <div className='study-time'><b>Study Time:</b> {time.toFixed(2)} hour</div>
+        <button>Add To List</button>
     </div>
   )
 }
--- src/components/StudyRoom/StudyRoom.js
+++ src/components/StudyRoom/StudyRoom.js
@@ -4,26 +4,28 @@ import './studyroom.css'
 
 const StudyRoom = () => {
     const [courses, setCourses] = useState([]);
-    
-    useEffect(()=>{
+
+    useEffect(() => {
         fetch('data.json')
             .then(res => res.json())
             .then(data => setCourses(data))
-    },[])
-    
+    }, [])
+
     return (
-    <div>
-        <div className="course-container">
-            <h2>Study Hour</h2>
-            {
-                courses.map(course => <Course course={course}></Course>)
-            }
+        <div className='study-room'>
+            <div className="course-container">
+                <h2>Study Hour</h2>
+                <div className="courses">
+                    {
+                        courses.map(course => <Course course={course}></Course>)
+                    }
+                </div>
+            </div>
+            <div className="cart-container">
+                <h2>Shahtaz Rahman</h2>
+            </div>
         </div>
-        <div className="cart-container">
-            <h2>Shahtaz Rahman</h2>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default StudyRoom
